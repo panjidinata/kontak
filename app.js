fdf96e571011a68e66771b92852584f11e4b20f2
Panji Dinata <panjidinata88@gmail.com>
2024-05-23 21:32:00 +0800

    implement method-override middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 const port = 3000;
 
 const Database = require("better-sqlite3");
@@ -44,6 +45,16 @@ console.log(listContact);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      let method = req.body._method;
+      console.log(method, req.body._method);
+      delete req.body._method;
+      return method;
+    }
+  }),
+);
 
 app.set("view engine", "pug");
 
