5da2dcf76e04fe7c3da487b71ae3b9553f96b122
Panji Dinata <panjidinata88@gmail.com>
2024-05-23 10:33:29 +0800

    create example route and data

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let listContact = [{ Name: "asdflkj", email: "adsfkjh" }];
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
+});
+
+app.get("/contact", (req, res) => {
+  res.json(listContact);
+});
+
+app.post("/contact", (req, res) => {
+  res.json("add contact");
+});
+
+app.get("/contact/:id", (req, res) => {
+  res.json("param: " + req.params.id);
+});
+
+app.put("/contact/:id", (req, res) => {
+  res.json("update contact");
+});
+
+app.delete("/contact/:id", (req, res) => {
+  res.json("delete contact");
+});
+
 app.listen(port, () => {
   console.log("listening on port: " + port);
 });
