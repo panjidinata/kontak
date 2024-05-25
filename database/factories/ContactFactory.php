eb9b29537877feabfb7942b09d5ae416565c79fb
Panji Dinata <panjidinata88@gmail.com>
2024-05-25 13:41:02 +0800

    generate fake name and email

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -16,6 +16,9 @@ class ContactFactory extends Factory
      */
     public function definition(): array
     {
+        return [
+            "name" => fake()->name();
+            "email" => fake()->email()->safeEmail();
         ];
     }
 }
