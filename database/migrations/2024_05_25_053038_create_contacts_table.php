ea6f16845e2fd01f09d3be4712b3eb5668c8b7aa
Panji Dinata <panjidinata88@gmail.com>
2024-05-25 14:00:13 +0800

    set email to unique

--- database/migrations/2024_05_25_053038_create_contacts_table.php
+++ database/migrations/2024_05_25_053038_create_contacts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             'contacts', function (Blueprint $table) {
                 $table->id();
                 $table->string("name");
-                $table->string("email");
+                $table->string("email")->unique();
                 $table->timestamps();
             }
         );
