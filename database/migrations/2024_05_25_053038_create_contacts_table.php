b2b5d69df12830ce2c2dc60e13b49b037ec1df69
Panji Dinata <panjidinata88@gmail.com>
2024-05-25 13:38:14 +0800

    add name and email column to create_contact_table migration

--- database/migrations/2024_05_25_053038_create_contacts_table.php
+++ database/migrations/2024_05_25_053038_create_contacts_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contacts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::create(
+            'contacts', function (Blueprint $table) {
+                $table->id();
+                $table->string("name");
+                $table->string("email");
+                $table->timestamps();
+            }
+        );
     }
 
     /**
